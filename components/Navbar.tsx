65e7998c043cd1e7e975536f9baa5b81872960f2
manab-debnath <manab_debnath@outlook.com>
2025-03-01 16:14:46 +0530

    Update Navbar component to import AnimatePresence from motion/react

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "motion/react";
 import { useScrollDirection } from "../hooks/useScrollDirection";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
